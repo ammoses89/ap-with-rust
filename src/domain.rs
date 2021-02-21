0d700b3351ded1d223cf9b4bea5753537c701543
Adrian Moses <adrian@adrianmoses.com>
2021-02-20 21:33:21 -0500

    takign a break;rust is confusing

--- src/domain.rs
+++ src/domain.rs
@@ -1,15 +1,20 @@
 use chrono::{Date, Local};
+use std::collections::HashSet;
+use std::cmp::Ordering;
+use std::ops::{DerefMut, Deref};
 
 pub trait Allocator {
-    fn allocate(&self, order_quantity: i32);
+    fn allocate(&mut self, order_line: &OrderLine);
+    fn deallocate(&mut self, order_line: &OrderLine);
 }
 
 pub struct Batch {
     reference: String,
     sku: String,
-    quantity: i32,
+    pub(crate) quantity: i32,
     eta: Date<Local>,
     is_shipping: bool,
+    allocations: HashSet<String>,
 }
 
 pub struct Order {
@@ -18,26 +23,94 @@ pub struct Order {
 }
 
 pub struct OrderLine {
+    order_reference: String,
     sku: String,
     quantity: i32,
 }
 
+impl OrderLine {
+    pub fn new(order_reference: &str, sku: &str, quantity: i32) -> Self {
+        OrderLine {
+            order_reference: String::from(order_reference),
+            sku: String::from(sku),
+            quantity,
+        }
+    }
+}
+
 impl Batch {
     pub fn new(reference: &str, sku: &str, quantity: i32, is_shipping: bool) -> Self {
+        let allocations = HashSet::new();
         Batch {
             reference: String::from(reference),
             sku: String::from(sku),
             quantity,
             eta: Local::now().date(),
-            is_shipping
+            is_shipping,
+            allocations,
         }
     }
 }
 
+
 impl Allocator for Batch {
-    fn allocate(&mut self, order_quantity: i32) {
-        if order_quantity < self.quantity {
-            self.quantity -= order_quantity;
+    fn allocate(&mut self, order_line: &OrderLine) {
+        let order_id = order_line.order_reference.as_str();
+
+        if self.sku != order_line.sku || self.allocations.contains(order_id) {
+            return;
+        }
+
+        if order_line.quantity <= self.quantity {
+            self.quantity -= order_line.quantity;
+            self.allocations.insert(order_id.to_string());
         }
     }
+
+    fn deallocate(&mut self, order_line: &OrderLine) {
+        let order_id = order_line.order_reference.as_str();
+
+        if self.sku == order_line.sku && self.allocations.contains(order_id) {
+            self.quantity += order_line.quantity;
+            self.allocations.remove(order_id);
+        }
+
+
+    }
+}
+
+impl Ord for Batch {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.is_shipping, self.eta).cmp(&(other.is_shipping, other.eta))
+    }
+}
+
+impl PartialOrd for Batch {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Batch {
+    fn eq(&self, other: &Self) -> bool {
+        (self.is_shipping, &self.eta) == (other.is_shipping, &other.eta)
+    }
+}
+
+impl Eq for Batch { }
+
+pub fn allocate(order_line: &OrderLine, mut batches: Vec<Batch>) -> String {
+    let batch_reference= String::from("");
+    batches
+        .sort_by(|b1, b2| b1.cmp(&b2));
+
+
+    let preferred_batch_option = batches.iter_mut().next();
+
+    if preferred_batch_option.is_some() {
+        let preferred_batch = preferred_batch_option.unwrap();
+        preferred_batch.allocate(order_line);
+    }
+
+    return batch_reference;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,94 @@ mod domain;
 #[cfg(test)]
 mod tests {
     use crate::domain;
+    use crate::domain::{Allocator, allocate};
 
     #[test]
     fn test_allocation_with_sufficient_batch_quantity() {
         // Create a batch with quantity
-        let batch = domain::Batch::new("ref-id", "sku-hi", 10, true);
+        let mut batch = domain::Batch::new("ref-id", "small-table", 20, true);
+        let order_line = domain::OrderLine::new("order-ref-id", "small-table", 2);
 
         // Create order with quantity
+        batch.allocate(&order_line);
+
         // Ensure allocation subtracts from batch quantity
+        assert_eq!(batch.quantity, 18);
     }
 
     #[test]
     fn test_allocation_with_insufficient_batch_quantity() {
+
         // Create a batch with quantity
+        let mut batch = domain::Batch::new("ref-id", "blue-cushion", 1, true);
         // Create order with quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-cushion", 2);
+
         // Ensure allocation subtracts from batch quantity
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 1);
     }
 
     #[test]
     fn test_allocation_when_order_allocation_called_twice() {
         // Create a batch with quantity
+        let mut batch = domain::Batch::new("ref-id", "blue-vase", 10, true);
         // Create order with quantity
-        // Ensure allocation subtracts from batch quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-vase", 2);
+
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 8);
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 8);
+    }
+
+    #[test]
+    fn test_cannot_allocate_if_skus_do_not_match() {
+        // Create a batch with quantity
+        let mut batch = domain::Batch::new("ref-id", "blue-vase", 10, true);
+        // Create order with quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-car", 2);
+
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 10);
+    }
+
+    #[test]
+    fn test_can_allocate_if_quantities_are_equal() {
+        // Create a batch with quantity
+        let mut batch = domain::Batch::new("ref-id", "blue-vase", 10, true);
+        // Create order with quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-vase", 10);
+
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 0);
+    }
+
+    #[test]
+    fn test_can_deallocate_unallocated_lines() {
+        // Create a batch with quantity
+        let mut batch = domain::Batch::new("ref-id", "blue-vase", 10, true);
+        // Create order with quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-vase", 10);
+
+        batch.allocate(&order_line);
+        assert_eq!(batch.quantity, 0);
+
+        batch.deallocate(&order_line);
+        assert_eq!(batch.quantity, 10);
+    }
+
+    #[test]
+    fn test_allocates_warehouse_batches_before_shipping_batches() {
+        // Create a batch with quantity
+        let shipping_batch = domain::Batch::new("ref-id", "blue-vase", 10, true);
+        let  warehouse_batch = domain::Batch::new("ref-id", "blue-vase", 10, false);
+        // Create order with quantity
+        let order_line = domain::OrderLine::new("order-ref-id", "blue-vase", 10);
+
+        allocate(&order_line, vec![shipping_batch, warehouse_batch]);
+        assert_eq!(shipping_batch.quantity, 10);
+        assert_eq!(warehouse_batch.quantity, 0);
+
     }
 }
